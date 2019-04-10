10f21b8fc93c208efb43d81fed72e0823ec4250a
dianebainbridge <diane.bainbridge@gmail.com>
2019-04-10 19:34:04 -0400

    Adding missed files in last commit

--- app/Http/Controllers/FuelConsumptionController.php
+++ app/Http/Controllers/FuelConsumptionController.php
@@ -7,16 +7,21 @@ use App;
 
 class FuelConsumptionController extends Controller
 {
+    /*
+    *  GET index
+    */
     public function index()
     {
+        #View the form
         return view('fuelConsumptionCalculator.form');
     }
 
     /*
-     * GET /books/getForm
+     * GET  showForm
      */
     public function showForm(Request $request)
     {
+        # Add variables to the session and view the form
         $startDistance = $request->session()->get('startDistance', 0);
         $endDistance = $request->session()->get('endDistance', 0);
         $fuelVolume = $request->session()->get('fuelVolume', 0);
@@ -35,7 +40,7 @@ class FuelConsumptionController extends Controller
     }
 
     /*
-     * GET //formProcess
+     * GET formProcess
      */
     public function formProcess(Request $request)
     {
@@ -48,7 +53,7 @@ class FuelConsumptionController extends Controller
             'volumeUnit' => 'required'
         ]);
 
-        # Store the startDistance, endDistance and fuelVolume in a variable for easy access
+        # Store the startDistance, endDistance and fuelVolume in variables
         # The second parameter (null) is what the variable
         # will be set to *if* the value is not in the request.
 
@@ -59,7 +64,7 @@ class FuelConsumptionController extends Controller
         $volumeUnit = $request->input('volumeUnit', null);
         $addLog = $request->input('addLog', null);
 
-        #If startDistance, endDistance and fuelVolume are not nulland the request has no errors calculate fuel consumed
+        #If startDistance, endDistance and fuelVolume are not null and the request has no errors calculate fuel consumed
         $fuelConsumed = 0;
         if (!$request->hasErrors && $startDistance && $endDistance && $fuelVolume) {
             #calculate fuel consumed - this is intentionally without units so you could calculate miles/liter if desired
@@ -68,35 +73,42 @@ class FuelConsumptionController extends Controller
         }
 
         #If the add log checkbox is checked add the data to the json file
-        #If a cookie is already set get the fuelCalc d for this user and add to log with user's id
-        if($addLog == "on")
-        {
+        if ($addLog == "on") {
             $fuelCalcID = "";
+            #If a cookie is already set get the fuelCalc d for this user and add to log with user's id
             if (isset($_COOKIE["fuelCalcID"])) {
                 $fuelCalcID = $_COOKIE["fuelCalcID"];
-            }
-            #create a fuelID for this user and set cookie
+            } #otherwise create a fuelCalcID for this user and set cookie
             else {
                 $fuelCalcID = uniqid();
                 setcookie("fuelCalcID", $fuelCalcID, time() + 180 * 24 * 60 * 60, "/", ".p3.dianebainbridge.com");
             }
+            #construct the fuel consumed text
             $fuelConsumption = $fuelConsumed . " " . $distanceUnit . " / " . $volumeUnit;
             $today = date("m/d/y");
-            $data = (object) array(
+            #create object from the form data
+            $data = (object)array(
                 'fuelCalcID' => $fuelCalcID,
                 'date' => $today,
                 'distance' => $distance,
                 'fuel' => $fuelVolume,
                 'fuelConsumption' => $fuelConsumption,
             );
+            #get contents of json file
             $inp = file_get_contents(database_path('/fuelLog.json'));
+            #decodde to a temporary array
             $tempArray = json_decode($inp);
+            #add the object to the array
             array_push($tempArray, $data);
+            #encode array
             $jsonData = json_encode($tempArray);
+            #put the data back in the json file
             file_put_contents(database_path('/fuelLog.json'), $jsonData);
+            #and return to the view log page to display the all the data
             return redirect('/fuelConsumptionCalculator/get-fuel-log');
         }
-        # Redirect the user page to the page that shows the form
+        # Redirect the user page to the page that shows the form to show the calculation
+        # if the user did not select add to log
         return redirect('/fuelConsumptionCalculator/show-form')->with([
             'fuelConsumed' => $fuelConsumed,
             'startDistance' => $startDistance,
@@ -114,20 +126,21 @@ class FuelConsumptionController extends Controller
         if (isset($_COOKIE["fuelCalcID"])) {
             $fuelCalcID = $_COOKIE["fuelCalcID"];
         }
-        $log = [];
         #If the user has added entries to the log in the last 6 months they should have a cookie set
-        #Note ideally I would use a database here but we have covered databases yet so I am using cookies which may not always be available
+        #Note ideally I would use a database here but we have covered databases yet so I am using cookies
+        # which may not always be available
+
         if ($fuelCalcID != "") {
-            # Start with an empty array of log entries that
-            # match the cookie value
+            # Start with an empty array of log entries
             $log = [];
+
             # Open the fuelLog.json data file
             $fuelLogData = file_get_contents(database_path('/fuelLog.json'));
 
             # Decode the book JSON data into an array
             $logEntries = json_decode($fuelLogData, true);
 
-            # Loop through all the log data, looking for matches
+            # Loop through all the log data, looking for matches for the user's cookie fuelCalcID
             foreach ($logEntries as $logEntry) {
                 if ($logEntry['fuelCalcID'] == $fuelCalcID) {
                     # If it was a match, add it to our results
--- resources/views/fuelConsumptionCalculator/form.blade.php
+++ resources/views/fuelConsumptionCalculator/form.blade.php
@@ -9,124 +9,128 @@
 @endsection
 
 @section('content')
-    <table>
-        <tr>
-            <td style="text-align:left;vertical-align:top;width:600px">
-                <p class="subHeader">Use the steps below to calculate your fuel consumption.</p>
-                <ol>
-                    <li>Enter the odometer reading from the last time you filled your gas tank.</li>
-                    <li>Enter the odometer reading at the time of your current fill up.</li>
-                    <li>Enter the amount of fuel required to completely fill your tank at this fill up.</li>
-                    <li>Be sure to select the units you are using to measure your distance and the amount of gas.</li>
-                </ol>
-                <p class="note">
-                    Note: before you use this the first time completely fill up your gas tank
-                    and save the distance read from your odometer.
-                </p>
-            </td>
-            <td>
-                <form method='GET' action='/fuelConsumptionCalculator/form-process'>
-                    <div class="form-group">
-                        <i class="fas fa-tachometer-alt"></i><!--fontawesome kind of odometer icon-->
-                        <label for="startDistance">Odometer reading - last fill-up </label>
-                        <input id="startDistance" name="startDistance" type="number"
-                               value="{{ old('startDistance') }}"/>
-                        @if($errors->get('startDistance'))
-                            <div class='error'>{{ $errors->first('startDistance') }}</div>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <i class="fas fa-tachometer-alt"></i>
-                        <label for="endDistance">Odometer reading - this fill-up </label>
-                        <input id="endDistance" name="endDistance" type="number" value="{{ old('endDistance') }}"/>
-                        @if($errors->get('endDistance'))
-                            <div class='error'>{{ $errors->first('endDistance') }}</div>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="distanceUnit">Select miles or kilometers</label>
-                        <div class="custom-control distanceUnit">
-                            <label for="miles">Miles
-                                <input type="radio" name="distanceUnit" id="miles" value="miles" class="custom-control-input"
-                                       @if (old('distanceUnit')=="miles")
-                                       checked = "checked"
-                                        @endif
-                                />
-                            </label>
-                        </div>
-                        <div class="custom-control  distanceUnit">
-                            <label class="custom-control-label" for="kilometers">Kilometers
-                                <input type="radio" id="kilometers" name="distanceUnit" class="custom-control-input"
-                                       @if (old('distanceUnit')=="kilometers")
-                                       checked = "checked"
-                                        @endif
-                                >
-                            </label>
-                        </div>
-                        @if($errors->get('distanceUnit'))
-                            <div class='error'>{{ $errors->first('distanceUnit') }}</div>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <i class="fas fa-gas-pump"></i>
-                        <label for="fuelVolume">Fuel Reading from Gas Pump </label>
-                        <input id="fuelVolume" name="fuelVolume" type="text" value="{{old('fuelVolume')}}"/>
-                        @if($errors->get('fuelVolume'))
-                            <div class='error'>{{ $errors->first('fuelVolume') }}</div>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="volumeUnit">Select option</label>
-                        <select id="volumeUnit" name="volumeUnit" class="custom-select volumeUnit">
-                            <option value="">&#160;</option>
-                            <option value="gallon"
-                                    @if (old('volumeUnit')=="gallon")
-                                        selected
-                                    @endif
-                            >
-                                Gallons
-                            </option>
-                            <option value="liter"
-                                    @if (old('volumeUnit')=="liter")
-                                    selected
-                                    @endif
-                            >
-                                Liters
-                            </option>
-                        </select>
-                        @if($errors->get('volumeUnit'))
-                            <div class='error'>{{ $errors->first('volumeUnit') }}</div>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="addLog">Add to log</label>
-                        <input type="checkbox" class="form-check-input" id="addLog" name="addLog"
-                               @if (old('addLog')=="on")
-                               checked = "checked"
+    <div class="row">
+        <div class="col-sm-6">
+            <p class="subHeader">Use the steps below to calculate your fuel consumption.</p>
+            <ol>
+                <li>Enter the odometer reading from the last time you filled your gas tank.</li>
+                <li>Enter the odometer reading at the time of your current fill up.</li>
+                <li>Enter the amount of fuel required to completely fill your tank at this fill up.</li>
+                <li>Be sure to select the units you are using to measure your distance and the amount of gas.</li>
+            </ol>
+            <p class="note">
+                Note: before you use this the first time completely fill up your gas tank
+                and save the distance read from your odometer.
+            </p>
+        </div>
+        <div class="col-sm-6">
+            <form method='GET' action='/fuelConsumptionCalculator/form-process'>
+                <div class="form-group">
+                    <i class="fas fa-tachometer-alt"></i><!--fontawesome kind of odometer icon-->
+                    <label for="startDistance">Odometer reading - last fill-up </label>
+                    <input id="startDistance" name="startDistance" type="number" value="{{ old('startDistance') }}"/>
+                    {{-- Display the first error if there are any fof this input --}}
+                    @if($errors->get('startDistance'))
+                        <div class='error'>{{ $errors->first('startDistance') }}</div>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <i class="fas fa-tachometer-alt"></i>
+                    <label for="endDistance">Odometer reading - this fill-up </label>
+                    <input id="endDistance" name="endDistance" type="number" value="{{ old('endDistance') }}"/>
+                    {{-- Display the first error if there are any fof this input --}}
+                    @if($errors->get('endDistance'))
+                        <div class='error'>{{ $errors->first('endDistance') }}</div>
+                    @endif
+                </div>
+                <div class="form-group">
+                    Select miles or kilometers
+                    <br/>
+                    <label class="radio-inline" for="miles">
+                        <input type="radio" name="distanceUnit" value="miles"
+                               {{-- If the old value i "miles" check this radio input--}}
+                               @if (old('distanceUnit')=="miles")
+                               checked="checked"
+                                @endif
+                        >Miles
+                    </label>
+                    <label class="radio-inline" for="kilometers">
+                        <input type="radio" id="kilometers" name="distanceUnit" value="kilometers"
+                               {{-- If the old value i "kilometers" check this radio input--}}
+                               @if (old('distanceUnit')=="kilometers")
+                               checked="checked"
+                                @endif
+                        >Kilometers
+                    </label>
+                    {{-- Display the first error if there are any fof this input --}}
+                    @if($errors->get('distanceUnit'))
+                        <div class='error'>{{ $errors->first('distanceUnit') }}</div>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <i class="fas fa-gas-pump"></i>
+                    <label for="fuelVolume">Fuel Reading from Gas Pump </label>
+                    <input id="fuelVolume" name="fuelVolume" type="text" value="{{old('fuelVolume')}}"/>
+                    @if($errors->get('fuelVolume'))
+                        <div class='error'>{{ $errors->first('fuelVolume') }}</div>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="volumeUnit">Select option</label>
+                    <select id="volumeUnit" name="volumeUnit" class="custom-select volumeUnit">
+                        <option value="">&#160;</option>
+                        <option value="gallon"
+                                {{-- If the old value is "gallon" select this option--}}
+                                @if (old('volumeUnit')=="gallon")
+                                selected
                                 @endif
                         >
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Calculate</button>
-                    </div>
-                    <div class="error">
-                        @if(count($errors)==0)
-                            @if(!empty($fuelConsumed))
-                                <p class="calculationResult">
-                                    {{$fuelConsumed}}  {{$distanceUnit}}/{{$volumeUnit}}
-                                </p>
-                            @endif
-                        @else
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
+                            Gallons
+                        </option>
+                        <option value="liter"
+                                {{-- If the old value is "liter" select this option--}}
+                                @if (old('volumeUnit')=="liter")
+                                selected
+                                @endif
+                        >
+                            Liters
+                        </option>
+                    </select>
+                    {{-- Display the first error if there are any fof this input --}}
+                    @if($errors->get('volumeUnit'))
+                        <div class='error'>{{ $errors->first('volumeUnit') }}</div>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="addLog" class="checkbox-inline">
+                        <input type="checkbox" id="addLog" name="addLog"
+                               {{-- If the old value is "on" select this option--}}
+                               @if (old('addLog')=="on")
+                               checked="checked"
+                                @endif
+                        ><b>Add to log</b>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Calculate</button>
+                </div>
+                {{-- Display the fuel calculation if there are no errors or the erorrs if there are errors--}}
+                <div class="outcome">
+                    @if(count($errors)==0)
+                        @if(!empty($fuelConsumed))
+                            <p class="calculationResult">
+                                Fuel Consumed : {{$fuelConsumed}}  {{$distanceUnit}}/{{$volumeUnit}}
+                            </p>
                         @endif
-                    </div>
-                </form>
-            </td>
-        </tr>
-    </table>
-
+                    @else
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
